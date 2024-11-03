e2881678fc7f0542acd0393289661a8fdbcc8d75
camila <camilacabrera464@gmail.com>
2024-11-02 21:46:39 -0500

    listo CRUD Tecnica

--- obj/Debug/net8.0/Api_Karate_Pro.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api_Karate_Pro.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Karate_Pro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915b2374ea34c49f3dc2a897a82db9a8c26c651f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3611a9a625f86b2d6630338724ed9346871682d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Karate_Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Karate_Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
